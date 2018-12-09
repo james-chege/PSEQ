d71709b9518da7153ec3c60eb675652a355d3d9f
james-chege <jymeschege@gmail.com>
2018-12-09 21:36:07 +0300

    Add primary key

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const Sequelize = require('sequelize');
 const app = express();
 const port = 8001;
 
-// app.listen(port, () => {
-//   console.log('Running server on port ' + port)
-// });
+app.listen(port, () => {
+  console.log('Running server on port ' + port)
+});
 
 const connection = new Sequelize('db', 'user', 'pass', {
   host: 'localhost',
@@ -16,14 +16,25 @@ const connection = new Sequelize('db', 'user', 'pass', {
 })
 
 const User  = connection.define('User', {
+  uuid: {
+    type: Sequelize.UUID,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4
+  },
   name: Sequelize.STRING,
   bio: Sequelize.TEXT
 })
 
 connection
   .sync({
-    logging: console.log
-  }).then()
+    logging: console.log,
+    force: true
+  }).then(() => {
+    User.create({
+      name: 'Joe',
+      bio: 'New bio entry'
+    })
+})
   .then(
   () => {
     console.log('Connect to the server established successfully.');
